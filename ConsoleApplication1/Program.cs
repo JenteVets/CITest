11fcce848d1f0019ce4625de60a871f51b41f082
Jente Vets <jentevets@gmail.com>
2015-09-01 17:08:16 +0200

    Initial commit

---
+++ ConsoleApplication1/Program.cs
@@ -0,0 +1,30 @@
+﻿using log4net;
+using log4net.Config;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        // Define a static logger variable
+        private static readonly ILog log = LogManager.GetLogger(typeof(Program));
+
+        static void Main(string[] args)
+        {
+            // Set up a simple configuration that logs on the console.
+            //BasicConfigurator.Configure();
+            XmlConfigurator.Configure();
+
+            log.Info("Entering application.");
+            Console.WriteLine("Dit is een test.");
+            Console.WriteLine("Logger installeren.");
+            log.Debug("De logger werkt.");
+            Console.ReadLine();
+            log.Info("Exiting application.");
+        }
+    }
+}
